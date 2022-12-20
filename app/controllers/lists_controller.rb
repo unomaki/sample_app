149cc1d4b845fc37057978ed2ad81106b975d19a
EC2 Default User <ec2-user@ip-172-31-42-242.ap-northeast-1.compute.internal>
2022-12-20 11:47:15 +0000

    [Finish]3-フラッシュメッセージを実装してみよう

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,15 +3,15 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
-
   def create
-    @list = List.new(list_params)
-    if @list.save
-    redirect_to list_path(@list.id)
-  else
-    render :new
+    # １. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
   end
-end
 
   def index
      @lists = List.all
